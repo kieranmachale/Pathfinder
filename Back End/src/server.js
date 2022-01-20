9ad8294d37bc44be2209a4057ed771565771a3c1
kieranmachale <c16777089@mytudublin.ie>
2022-01-20 15:35:33 +0000

    updated server

--- Back End/src/server.js
+++ Back End/src/server.js
@@ -1,21 +1,46 @@
 const express = require('express');
 const cors = require('cors');
+const bodyParser = require("body-parser");
 const server = express();
 const PORT = process.env.PORT || 3000;
 const url = process.env.URL || "http://localhost";
 
+server.use(bodyParser.urlencoded({ extended: true }));
+server.use(bodyParser.json());
 // ---------------------------------------------------------------
 
 /* cors settings */
 let corsOptions = {
-    origin: url + ":" + 8080
+    origin: [url + ":" + 8080, '*']
 }
+
+let allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', "*");
+    res.header('Access-Control-Allow-Headers', "*");
+    next();
+}
+server.use(allowCrossDomain);
 server.use(cors(corsOptions));
+
+// ----------------------------------------------------------------
 
 /* Endpoint to check server is active */
 server.get("/", cors(), (req, res) => {
-    res.json('Server is running!').status(200);
-})
+    res.json('Server is responsive').status(200);
+});
+
+/* Endpoint to check fetch API GET requests */
+server.get("/test-get", cors(), (req, res) => {
+    const data = {obj: {id:1, name:'obj1'}, obj2: {id:2, name:'obj2'}};
+    res.json(data).status(200);
+});
+
+
+/* Endpoint to check fetch API POST requests */
+server.post("/test-post", cors(), (req, res) => {
+    console.log(req.body);
+    res.json(req.body).status(200);
+});
 
 /* Start server */
 server.listen(PORT, () => {
--- Front End/javascript/test.js
+++ Front End/javascript/test.js
@@ -2,20 +2,53 @@
 
 document.querySelector("#btn1").addEventListener("click", function(e){
     //console.log("Button clicked");
-    fetch('https://reqres.in/api/users', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-          'Accept': 'application/json'  
-        },
+    fetch('http://localhost:3000/test-get', {
+        method: 'GET'
+        /*,
         body: JSON.stringify({
             name: document.querySelector("#email").value,
             password: document.querySelector("#pass").value
-        })
+        })*/
     }).then(res => {
+        console.log(res.status);
         return res.json();
     }).then(data => {
         console.log(data);
     }).catch(err => console.log(err));
 });
 
+/*
+document.querySelector("#logo").addEventListener("click", function(e){
+    fetch('http://localhost:3000/blu', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+            'Access-Control-Allow-Origin' :'*',
+            'Access-Control-Allow-Methods' : 'GET, POST, PUT, DELETE, OPTIONS'
+        },
+        body: JSON.stringify({
+            a: 7,
+            str: 'Some string &&&'
+        })
+    }).then(res => {
+        console.log(res.status);
+    }).catch(err => console.log(err));
+});*/
+
+document.querySelector("#logo").addEventListener("click", function(e){
+    fetch('http://localhost:3000/test-post', {
+	method: 'POST',
+    headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+    },
+    body: JSON.stringify({
+  	    name: 'Kieran',
+        email: 'machale@gmail.com'
+    })
+	})
+	.then(res => res.json())
+    .then(data => console.log(data))
+    .catch(err => console.log(err));
+});
