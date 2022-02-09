44add076bca5f9e004bd790809cb4ce6b3c3ab07
kieranmachale <c16777089@mytudublin.ie>
2022-02-09 19:47:24 +0000

    implemented middleware for registration checking

--- pathfinder-app/src/routes/auth.routes.js
+++ pathfinder-app/src/routes/auth.routes.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const authenticationController = require('../controllers/auth.controller');
+const registerCheck = require('../middleware/registerCheck');
 
 module.exports = authenticationRouter => {
 
@@ -16,7 +17,7 @@ module.exports = authenticationRouter => {
     });
 
     // Register a new user
-    router.post("/register", authenticationController.register);
+    router.post("/register", [registerCheck.checkUniqueUsernameEmail], authenticationController.register);
 
     // Login to user account
     router.post("/login", authenticationController.login);
